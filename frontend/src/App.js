06e83e255c80f7b8e64c3d5d68e08919de97fcde
kksolanki1503 <kksolanki1503@gmail.com>
2024-12-30 23:56:00 +0530

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import React, { useEffect, useState, useRef } from "react";
 import io from "socket.io-client";
 import "./App.css";
-
-const SERVER_URL = "https://thadi.in";
+import AppConstant from "./AppConstant/AppConstant";
+import { Mic } from "lucide-react";
+import { MicOff } from "lucide-react";
+import { Video } from "lucide-react";
+import { VideoOff } from "lucide-react";
+const SERVER_URL = AppConstant.baseUrl;
 const socket = io(SERVER_URL);
+
 function App() {
   // const [localStream, setLocalStream] = useState(null);
   let localStreamRef = useRef(null);
@@ -11,6 +16,8 @@ function App() {
 
   const [connected, setConnected] = useState(false);
   const [isFinding, setIsFinding] = useState(false);
+  const [isVideo, setIsVideo] = useState(true);
+  const [isMic, setIsMic] = useState(true);
 
   // const [socket, setSocket] = useState(null);
   // const [peerConnection, setPeerConnection] = useState(null);
@@ -20,13 +27,21 @@ function App() {
 
   const localVideoRef = useRef();
   const remoteVideoRef = useRef();
+  const [isStarted, setIsStarted] = useState(false);
 
   const startCall = () => {
     setIsFinding(true);
+    setIsStarted(true);
     socket.emit("findPeer");
   };
 
   const createPeerConnection = async () => {
+    if (peerConnectionRef.current) {
+      console.log("Existing peer connection found, closing...");
+      peerConnectionRef.current.close();
+      peerConnectionRef.current = null;
+    }
+
     const configuration = {
       iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
     };
@@ -56,19 +71,33 @@ function App() {
 
         socket.on("answer", async ({ answer, from }) => {
           console.log("answer receiver", answer, "from ", from);
-          const remoteDesc = new RTCSessionDescription(answer);
-          await peerConnection.setRemoteDescription(remoteDesc);
+
+          if (peerConnectionRef.current.signalingState === "have-local-offer") {
+            const remoteDesc = new RTCSessionDescription(answer);
+            await peerConnectionRef.current.setRemoteDescription(remoteDesc);
+          } else {
+            console.warn(
+              "Cannot handle answer: Invalid signaling state",
+              peerConnectionRef.current.signalingState
+            );
+          }
         });
       } else if (partner.info.role === "o") {
         // Wait for an offer, create and send an answer
         socket.on("offer", async ({ offer, from }) => {
           console.log("offer receiver: ", offer, "from ", from);
-
-          await peerConnection.setRemoteDescription(offer);
-          const answer = await peerConnection.createAnswer();
-          await peerConnection.setLocalDescription(answer);
-          console.log("send answer to ", from);
-          socket.emit("answer", { answer, to: from });
+          if (peerConnectionRef.current.signalingState === "stable") {
+            await peerConnectionRef.current.setRemoteDescription(offer);
+            const answer = await peerConnectionRef.current.createAnswer();
+            await peerConnectionRef.current.setLocalDescription(answer);
+            console.log("send answer to ", from);
+            socket.emit("answer", { answer, to: from });
+          } else {
+            console.warn(
+              "Cannot handle offer: Invalid signaling state",
+              peerConnectionRef.current.signalingState
+            );
+          }
         });
       }
     } else {
@@ -163,6 +192,36 @@ function App() {
     }
   };
 
+  const toggleVideo = () => {
+    if (localStreamRef.current) {
+      const videoTrack = localStreamRef.current.getVideoTracks()[0];
+      if (videoTrack) {
+        setIsVideo(!videoTrack.enabled);
+        videoTrack.enabled = !videoTrack.enabled;
+        console.log(`Video is now ${videoTrack.enabled ? "ON" : "OFF"}`);
+      } else {
+        console.warn("No video track found");
+      }
+    } else {
+      console.warn("No local stream available");
+    }
+  };
+
+  const toggleMic = () => {
+    if (localStreamRef.current) {
+      const audioTrack = localStreamRef.current.getAudioTracks()[0];
+      if (audioTrack) {
+        setIsMic(!audioTrack.enabled);
+        audioTrack.enabled = !audioTrack.enabled;
+        console.log(`Microphone is now ${audioTrack.enabled ? "ON" : "OFF"}`);
+      } else {
+        console.warn("No audio track found");
+      }
+    } else {
+      console.warn("No local stream available");
+    }
+  };
+
   useEffect(() => {
     const setting = async () => {
       await createPeerConnection();
@@ -174,10 +233,29 @@ function App() {
 
   const endCall = () => {
     // localStreamRef = null;
+    if (peerConnectionRef.current) {
+      peerConnectionRef.current.close();
+
+      peerConnectionRef = null;
+    } else {
+      console.log("no peerREf found");
+    }
+    if (remoteVideoRef.current) remoteVideoRef.current.srcObject = null;
+    console.log("stream clear");
     remoteStreamRef = null;
-    setConnected(false);
-    peerConnectionRef.current.close();
+    console.log("partner clear");
     setPartner(null);
+    console.log("connection clear");
+    setConnected(false);
+
+    console.log("socket clear");
+    socket.off("offer");
+    socket.off("answer");
+    socket.off("ice-candidate");
+    console.log("isStarted clear");
+    setIsStarted(false);
+    console.log("isFinding clear");
+    setIsFinding(false);
   };
 
   useEffect(() => {
@@ -192,59 +270,88 @@ function App() {
     setting();
 
     // Return cleanup function
-    return () => {
-      if (peerConnectionRef.current) {
-        peerConnectionRef.current.close();
-      }
-    };
+    // return () => {
+    //   if (peerConnectionRef.current) {
+    //     peerConnectionRef.current.close();
+    //   }
+    // };
   }, []);
 
   return (
-    <div className="app max-w-[1990px] sm:px-[80px] px-[20px] min-h-screen bg-[#0B192C] nunito-font">
-      <div className="flex flex-col video-container sm:flex-row ">
-        <div className="w-full bg-[#1E3E62] ">
+    <div className="app max-w-[1990px] mx-auto px-[2.5vw] min-h-screen bg-[#212121] nunito-font  ">
+      <div className="flex flex-col items-center justify-center video-container sm:flex-row ">
+        <div className=" bg-[#161616]      relative sm:w-1/2 w-full flex items-center justify-center">
           <video
             ref={localVideoRef}
             autoPlay
             playsInline
             muted
-            className="object-cover w-full local-video"
+            className="object-contain  local-video h-[40vh]  sm:h-[80vh] "
           />
+          <div
+            className="absolute bottom-[2vw] left-[2vw] text-white   bg-black/70 px-[10px] py-[2px] rounded-full"
+            style={{ fontSize: "clamp(1rem,2.5vw,1.2rem)" }}
+          >
+            you
+          </div>
         </div>
-        <div className="w-full   bg-[#1E3E62] h-auto flex justify-center items-center text-white text-[2rem] relative">
+        <div className=" w-full sm:w-1/2 text-center   bg-[#161616] h-auto flex justify-center items-center text-white text-[2rem] relative">
           <video
             ref={remoteVideoRef}
             autoPlay
             playsInline
-            className={`w-full remote-video h-full  ${
+            className={` remote-video h-[40vh]  sm:h-[80vh] border  object-contain  ${
               !isFinding && connected ? "block" : "hidden"
             }`}
           />
           {isFinding ? (
-            <div className="h-auto">Finding</div>
+            <div className="h-[40vh]  sm:h-[80vh] flex items-center">
+              Finding
+            </div>
           ) : (
-            <div className={`${connected ? "hidden" : ""} h-auto`}>MikiTV</div>
+            <div
+              className={`${
+                connected ? "hidden" : ""
+              } h-[40vh]  sm:h-[80vh]  flex items-center`}
+            >
+              MikiTV
+            </div>
           )}
         </div>
       </div>
-      <div className="controls pt-[10px] flex gap-3">
-        <button
-          className="bg-[#FF6500] sm:px-[50px] px-[10px] sm:py-[10px] text-white sm:text-[3rem] text-[2rem] rounded-xl"
-          onClick={startCall}
-          disabled={connected}
-        >
-          Start
+
+      {/* controller */}
+      <div
+        className="controls pt-[10px] flex gap-3 justify-start items-center flex-wrap"
+        style={{ fontSize: "clamp(2rem,4vw,3rem)" }}
+      >
+        {isStarted ? (
+          <button
+            className="button"
+            // style={{padding:""}}
+            onClick={() => {
+              endCall();
+              startCall();
+            }}
+          >
+            Next
+          </button>
+        ) : (
+          <button className=" button" onClick={startCall}>
+            Start
+          </button>
+        )}
+
+        <button className="button" onClick={endCall}>
+          {isFinding ? `Stop` : `End`}
         </button>
-        <button
-          className="bg-[#FF6500] sm:px-[50px] px-[10px] sm:py-[10px] text-white sm:text-[3rem] text-[2rem] rounded-xl"
-          onClick={endCall}
-          disabled={!connected}
-        >
-          End
+
+        <button className="button" onClick={toggleVideo}>
+          {isVideo ? <Video /> : <VideoOff />}
         </button>
-        {/* <button onClick={nextUser} disabled={!connected}>
-          Next
-        </button> */}
+        <button className="button" onClick={toggleMic}>
+          {isMic ? <Mic /> : <MicOff />}
+        </button>
       </div>
     </div>
   );
